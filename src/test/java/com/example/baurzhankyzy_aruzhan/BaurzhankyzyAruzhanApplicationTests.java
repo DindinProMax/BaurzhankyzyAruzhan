e28defe3525161de1d24617d9d049562b3a3a781
DindinProMax <arukaadish@gmail.com>
2026-05-09 22:34:59 +0300

    Final project consolidation by Baurzhankyzy Aruzhan

---
+++ src/test/java/com/example/baurzhankyzy_aruzhan/BaurzhankyzyAruzhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baurzhankyzy_aruzhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaurzhankyzyAruzhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
